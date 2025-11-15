4ac1a254fc17eb7a7b647c1fbb36bb878ce78942
simplana <simplanaaisolutions@gmail.com>
2025-11-15 18:26:57 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { enforceHttps } from './utils/security';
+
+enforceHttps();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
