ddbc7d684a6733a37497f80a230090a0950cca9e
simplana <simplanaaisolutions@gmail.com>
2026-01-21 13:28:37 -0500

    Updated Restauraciones.tsx

--- src/pages/Restauraciones.tsx
+++ src/pages/Restauraciones.tsx
@@ -138,7 +138,7 @@ const Restauraciones = () => {
                       </div>
                       <div className="relative bg-gray-50 rounded-lg p-4">
                         <img
-                          src="https://images.pexels.com/photos/8989574/pexels-photo-8989574.jpeg?auto=compress&cs=tinysrgb&w=800"
+                          src="/image copy copy copy.png"
                           alt="Después de la restauración"
                           className="w-full h-auto object-contain mx-auto rounded-lg shadow-md"
                           style={{ maxHeight: '600px' }}
